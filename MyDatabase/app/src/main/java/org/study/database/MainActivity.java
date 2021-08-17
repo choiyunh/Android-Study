4c8f88c0c1a54567ab595837b78febe4fdffa9ce
Choiyunh_ <svs101@naver.com>
2021-08-17 18:58:12 +0900

    DB생성, 테이블 생성, 조회

---
+++ MyDatabase/app/src/main/java/org/study/database/MainActivity.java
@@ -0,0 +1,165 @@
+package org.study.database;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    EditText editText2;
+    TextView textView;
+
+    SQLiteDatabase database;
+
+    String tableName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String databaseName = editText.getText().toString();
+                createDatabase(databaseName);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableName = editText2.getText().toString();
+                createTable(tableName);
+            }
+        });
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertRecord();
+            }
+        });
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executeQuery();
+            }
+        });
+
+    }
+
+
+    private void createDatabase(String databaseName) {
+        println("createDatabase 호출됨.");
+
+        try {
+            database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null);
+            println("데이터베이스 생성함 : " + databaseName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createTable(String tableName) {
+        println("createTable 호출됨.");
+
+        try {
+            if (database == null) {
+                println("데이터베이스를 먼저 생성하세요.");
+                return;
+            }
+
+            database.execSQL("create table if not exists " + tableName + "("
+                    + " _id integer PRIMARY KEY autoincrement, "
+                    + " name text, "
+                    + " age integer, "
+                    + " mobile text)");
+
+            println("테이블 생성함 : " + tableName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertRecord() {
+        println("insertRecord 호출됨.");
+
+        try {
+            if (database == null) {
+                println("데이터베이스를 먼저 생성하세요.");
+                return;
+            }
+
+            if (tableName == null) {
+                println("테이블을 먼저 생성하세요.");
+                return;
+            }
+
+            database.execSQL("insert into " + tableName
+                    + "(name, age, mobile) "
+                    + " values "
+                    + "('John', 20, '010-1000-1000')");
+
+            println("레코드 추가함.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executeQuery() {
+        println("executeQuery 호출됨");
+
+        try {
+            if (database == null) {
+                println("데이터베이스를 먼저 생성하세요.");
+                return;
+            }
+
+            if (tableName == null) {
+                println("테이블을 먼저 생성하세요.");
+                return;
+            }
+
+            Cursor cursor = database.rawQuery("select _id, name, age, mobile from"
+                    + tableName, null);
+            int recordCount = cursor.getCount();
+            println("레코드 갯수 : " + recordCount);
+
+            for (int i = 0; i < recordCount; i++) {
+                cursor.moveToNext();
+
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int age = cursor.getInt(2);
+                String mobile = cursor.getString(3);
+
+                println("레코드 #" + i + " : " + id + ", " + name + ", " + age + ", " + mobile);
+            }
+
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+
+}
